be43c8ce15d95b381c180513f7b6c2730438ddd5
gayashan4lk <gayashan4lk@gmail.com>
2024-04-11 12:22:42 +0530

    add create api endpoint

--- src/test/java/com/gaya/cashcard/CashCardApplicationTests.java
+++ src/test/java/com/gaya/cashcard/CashCardApplicationTests.java
@@ -8,6 +8,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import java.net.URI;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -36,6 +37,24 @@ class CashCardApplicationTests {
 
         assertThat(response.getStatusCode()).isEqualTo(HttpStatus.NOT_FOUND);
         assertThat(response.getBody()).isBlank();
+    }
+
+    @Test
+    void shouldCreateNewCashCard(){
+        CashCard newCashCard = new CashCard(null, 250.00);
+        ResponseEntity<Void> response = restTemplate.postForEntity("/cashcards", newCashCard, void.class);
+
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.CREATED);
+
+        URI locationOfNewCashCard = response.getHeaders().getLocation();
+        ResponseEntity<String> getResponse = restTemplate.getForEntity(locationOfNewCashCard, String.class);
+        assertThat(getResponse.getStatusCode()).isEqualTo(HttpStatus.OK);
+
+        DocumentContext documentContext = JsonPath.parse(getResponse.getBody());
+        Number id = documentContext.read("$.id");
+        Double amount = documentContext.read("$.amount");
+        assertThat(id).isNotNull();
+        assertThat(amount).isEqualTo(250.00);
     }
 
 }
