21a17ed5f273f15539a961f90921c4c1a749b03c
gayashan4lk <gayashan4lk@gmail.com>
2024-04-11 13:05:40 +0530

    optimize imports

--- src/test/java/com/gaya/cashcard/CashCardJsonTest.java
+++ src/test/java/com/gaya/cashcard/CashCardJsonTest.java
@@ -8,7 +8,6 @@ import org.springframework.boot.test.autoconfigure.json.JsonTest;
 import org.springframework.boot.test.json.JacksonTester;
 
 import java.io.IOException;
-import java.lang.reflect.Array;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
